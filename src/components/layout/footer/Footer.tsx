c401072b71e59d15bb89f0f11462a476af87585a
tulobaev <talgattulobaev519>
2025-04-13 19:22:37 +0600

    .

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -3,11 +3,92 @@ import scss from "./Footer.module.scss";
 
 const Footer: FC = () => {
   return (
-    <section className={scss.Footer}>
+    <footer className={scss.Footer}>
       <div className="container">
-        <div className={scss.content}>Footer</div>
+        <div className={scss.content}>
+          <div className={scss.column}>
+            <h3>About Us</h3>
+            <ul>
+              <li>
+                <a href="#">Our Company</a>
+              </li>
+              <li>
+                <a href="#">Our Coffee</a>
+              </li>
+              <li>
+                <a href="#">Stories and News</a>
+              </li>
+              <li>
+                <a href="#">Investor Relations</a>
+              </li>
+            </ul>
+          </div>
+          <div className={scss.column}>
+            <h3>Careers</h3>
+            <ul>
+              <li>
+                <a href="#">Culture and Values</a>
+              </li>
+              <li>
+                <a href="#">Inclusion & Diversity</a>
+              </li>
+              <li>
+                <a href="#">College Achievement Plan</a>
+              </li>
+              <li>
+                <a href="#">Alumni Community</a>
+              </li>
+            </ul>
+          </div>
+          <div className={scss.column}>
+            <h3>Social Impact</h3>
+            <ul>
+              <li>
+                <a href="#">Ethical Sourcing</a>
+              </li>
+              <li>
+                <a href="#">Sustainability</a>
+              </li>
+              <li>
+                <a href="#">Community</a>
+              </li>
+              <li>
+                <a href="#">Global Social Impact</a>
+              </li>
+            </ul>
+          </div>
+          <div className={scss.column}>
+            <h3>For Business Partners</h3>
+            <ul>
+              <li>
+                <a href="#">Corporate Gift Card Sales</a>
+              </li>
+              <li>
+                <a href="#">Office and Foodservice Coffee</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={scss.bottom}>
+          <div className={scss.logo}>
+            <img
+              src="https://castlewestcork.ie/wp-content/uploads/2022/12/starbucks-logo.png"
+              alt="Starbucks Logo"
+            />
+          </div>
+          <div className={scss.copyright}>
+            <p>&copy; 2025 Starbucks Coffee Company. All rights reserved.</p>
+            <div className={scss.bottomLinks}>
+              <a href="#">Privacy Policy</a>
+              <a href="#">Terms of Use</a>
+              <a href="#">CA Supply Chain Act</a>
+              <a href="#">Cookie Preferences</a>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </footer>
   );
 };
 
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,11 +1,65 @@
 import { FC } from "react";
 import scss from "./Header.module.scss";
+import { links } from "../../../constants/Link";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header: FC = () => {
+  const navigate = useNavigate();
   return (
     <section className={scss.Header}>
       <div className="container">
-        <div className={scss.content}>Header</div>
+        <div className={scss.content}>
+          <div className={scss.menu}>
+            <div className={scss.logo}>
+              <img
+                onClick={() => navigate("/")}
+                src="https://castlewestcork.ie/wp-content/uploads/2022/12/starbucks-logo.png"
+                alt="logo"
+              />
+            </div>
+            <nav>
+              {links.map((item, index) => (
+                <Link key={index} className={scss.link} to={item.link}>
+                  {item.title}
+                </Link>
+              ))}
+            </nav>
+          </div>
+          <form className={scss.form}>
+            <button>
+              <svg width="10" height="16" fill="none" role="img">
+                <path
+                  d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+                  stroke="currentColor"
+                  stroke-width="1.333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </button>
+            <input
+              className={scss.input}
+              placeholder="Type your text"
+              type="text"
+            />
+            <button className={scss.reset} type="reset">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
